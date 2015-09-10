bb054ae9f74e251528210caabb81b6cc953b9fbf
Meredith Jones <meredithjones@Merediths-MacBook-Air.local>
2015-09-09 20:41:55 -0400

    initial

--- m8ball.py
+++ m8ball.py
@@ -6,7 +6,7 @@
 # my name is Michael Toth, my function is m8ballmt()
 
 # Mediapath should be set here for your machine
-mediaPath = "/Users/Kristina/csi106/magic8ball"
+mediaPath = "/users/meredithjones/csi106/magic8ball"
 setMediaPath(mediaPath)
 
 # FUNCTIONS
@@ -14,41 +14,29 @@ setMediaPath(mediaPath)
 # YOU WILL NEED TO CHANGE YOUR PATH FOR THE PICTURE FILE TO USE THE
 # SAME ONE AS IN m8ballmt()
 
-def m8ballkp():
-  f = "game_8ball_big.jpg"
-  p=makePicture(f)
-  s=makeStyle(sansSerif,bold,14)
-  addTextWithStyle(p,173,140,"MY",s,white)
-  addTextWithStyle(p,155,160,"SOURCES",s,white)
-  addTextWithStyle(p,175,180,"SAY",s,white)
-  addTextWithStyle(p,183,194,"NO",s,white)
-  repaint(p)
-
 def m8ballmt():
   f="magic-8-ball-6.jpg"
   p=makePicture(f)
   s=makeStyle(sansSerif,bold,18)
   addTextWithStyle(p,120,130,"NO",s,white)
   repaint(p)
-def m8ballkm():
+  
+def m8ballmj():
   f="magic-8-ball-6.jpg"
-  p=makePicture(f) 
-  s=makeStyle(sansSerif,bold,18) 
-  addTextWithStyle(p,115,100,"Yes",s,white) 
-  addTextWithStyle(p,85,130,"Definitely",s,white) 
-  repaint(p)
+  p=makePicture(f)
+  s=makeStyle(sansSerif,bold,18)
+  addTextWithStyle(p,60,90,"Cannot Predict",s,white)
+  s=makeStyle(sansSerif,bold,18)
+  addTextWithStyle(p,108,124,"Now",s,white)
+  repaint(p) 
 
 # END OF FUNCTIONS
 
 # ADD YOUR FUNCTION NAME TO THIS LIST.
 # For instance, if your function name is m8ballsw, [m8ballmt] should change
 # to [m8ballmt, m8ballsw]
-<<<<<<< HEAD
 
-phrases=[m8ballmt, m8ballkp]
-=======
-phrases=[m8ballmt, m8ballkm]
->>>>>>> a518cd7654681fba9daf80b85b9205f77f73f724
+phrases=[m8ballmt, m8ballmj]
 
 import random
 sel=random.randint(0,len(phrases)-1)
