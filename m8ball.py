1bab4e25a5ff756765e5ce6931f2ebdfdff194ce
kprozzo <kathrynprozzo@gmail.com>
2015-09-09 19:32:41 -0500

    answerkp

--- m8ball.py
+++ m8ball.py
@@ -14,6 +14,16 @@ setMediaPath(mediaPath)
 # YOU WILL NEED TO CHANGE YOUR PATH FOR THE PICTURE FILE TO USE THE
 # SAME ONE AS IN m8ballmt()
 
+def m8ballkp():
+  f = "game_8ball_big.jpg"
+  p=makePicture(f)
+  s=makeStyle(sansSerif,bold,14)
+  addTextWithStyle(p,173,140,"MY",s,white)
+  addTextWithStyle(p,155,160,"SOURCES",s,white)
+  addTextWithStyle(p,175,180,"SAY",s,white)
+  addTextWithStyle(p,183,194,"NO",s,white)
+  repaint(p)
+
 def m8ballmt():
   f="magic-8-ball-6.jpg"
   p=makePicture(f)
@@ -27,7 +37,7 @@ def m8ballmt():
 # For instance, if your function name is m8ballsw, [m8ballmt] should change
 # to [m8ballmt, m8ballsw]
 
-phrases=[m8ballmt]
+phrases=[m8ballmt, m8ballkp]
 
 import random
 sel=random.randint(0,len(phrases)-1)
